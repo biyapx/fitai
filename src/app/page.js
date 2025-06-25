58e991ced639f4294fd3af0e0f35ac7f84ff7be3
tibex88 <tibesolomon7@gmail.com>
2025-06-25 13:19:51 +0300

    fix: contact us import

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import { ApproachSection } from "@/components/Approach";
 import Hero from "@/components/Hero";
 import Solution from "@/components/Solution";
 import BenefitsSection from "@/components/Benefits";
-import ContactUs from "@/components/Contactus";
+import ContactUs from "@/components/ContactUs";
 
 export default function Home() {
   return (
